ac52f15b48240227184d57b852fcffd5991cb995
sajithv <sajith.vijesekara@gmail.com>
2023-07-04 02:50:19 +0530

    JWT Token Generation Logic Added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,10 +4,15 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import * as argon from 'argon2';
 import { Prisma } from '@prisma/client';
 import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable({})
 export class AuthService {
-  constructor(private prisma: PrismaService, private jwt: JwtService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async login(dto: AuthDto) {
     const user = await this.prisma.user.findUnique({
@@ -21,8 +26,7 @@ export class AuthService {
 
     if (!chkPassword) throw new ForbiddenException('Incorrect Password');
 
-    delete user.hash;
-    return user;
+    return this.signToken(user.id, user.email);
   }
 
   async signup(dto: AuthDto) {
@@ -49,5 +53,24 @@ export class AuthService {
     }
   }
 
-  signToken() {}
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email: email,
+    };
+
+    const secret = this.config.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: secret,
+    });
+
+    return {
+      access_token: token,
+    };
+  }
 }
